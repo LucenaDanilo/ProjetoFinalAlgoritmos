0a81908dc28186e47d3b68e239db4940572c3c7a
andrzingu <andregustavocarvalho2016@gmail.com>
2022-12-04 14:29:46 -0300

    #danilo heteros fanta

---
+++ main_andre_lindo.py
@@ -0,0 +1,48 @@
+"""3 por 4 """
+numero_colunas = 5
+numero_linhas = 6
+
+
+# FAZ A MATRIZ DO TABULEIRO
+def faz_tabuleiro():
+    matriz = []
+    for i in range(numero_linhas):
+        lista = []
+        for c in range(numero_colunas):
+            lista.append(" ")
+        matriz.append(lista)
+    return matriz
+# PRINTA O TABULEIRO
+def printar_tabuleiro(tabuleiro):
+    numeros_verticais = [1,2,3,3,2,1]
+    indice = 0
+    for i in range(len(tabuleiro)):
+        if i == 3:
+            print(f"{jogador_1} \n  x\n{jogador_2}")
+            print("―――――――――――――――――――――――――――")
+        if i == 0:
+            print("     Colunas", end="")
+            print(" "*10, end="")
+            print("A   B   C   D   E")
+            print("―――――――――――――――――――――――――――")
+        for c in range(5):
+            if c == 0:
+                print(f"Número da linha ({numeros_verticais[indice]}) | ", end="")
+                indice += 1
+                print(tabuleiro[i][c], end=" | ")
+            elif c<4:
+                print(tabuleiro[i][c], end=" | ")
+            else:
+
+                print(tabuleiro[i][c], end="")
+                print(" |")
+
+        print("―――――――――――――――――――――――――――")
+        if i == 5:
+            print("     Colunas", end="")
+            print(" " * 10, end="")
+            print("A   B   C   D   E")
+jogador_1 = input("Jogador 1 digite seu nome :""\n")
+jogador_2 = input("Jogador 2 digite seu nome :""\n")
+tabuleiro = faz_tabuleiro()
+printar_tabuleiro(tabuleiro)
