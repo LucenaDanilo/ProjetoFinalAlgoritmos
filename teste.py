e20821f91da1f9e28ecaa5b47c8af1ff07cfa48b
andrzingu <andregustavocarvalho2016@gmail.com>
2022-12-19 09:21:26 -0300

    Adionando os comentários de andre

--- teste.py
+++ teste.py
@@ -1,3 +1,57 @@
-from main_danilo import deck
+""""trabalhando com uma base de 30 cartas
+ideia inicial 8 guerreiros , 8 de medio alcance , 8 de longo alcance
+com 6 cartas sendo feitiços"""
+""""na ideia prinpipal havia 5 generais
+desses 5 generais optei por 2 deles serem professores henrique e iana, ambos professores de programação
+pensei em colocar os alunos como cartas
+como pensamos em uma via com dupla pro general com ele trazendo um buff e um debuff
+turma1 . danilo, andré e zé (henrique)
+turma2 . thiago , symon, luiza (iana)
+renata escolheu henrique ( danilo(+2 vid), *symon*(-2 de vida) e zé(+2 de vida))
+andre escolheu iana(thiago(+3 de atk), andré(-2 de atk), luiza(+3 de atk)"""
 
-print(deck)
+#################################################tabuleiro de renata - vida preestabelecida 200 vida
+"longo alcance"
+"medio alcance"
+"herois de corpo a corpo"
+##################################################terreno morto
+"herois de corpo a corpo"
+"medio alcance"
+"longo alcance"
+##################################################tabuleiro de andré
+"""em torno 10 rodadas 
+1 turno
+renta 
+andre
+1 turno de batalha
+checagem 
+vida maior que 0
+carta sobreviveu
+2 turno....."""
+"""" rei leonidas ("grito de leonidas")
+alexandre o grande("convocação de alexandre")
+gengis khan ("céu escuro") """
+""""herinque e iana("")
+"""
+""""atacar duas vezes (carta escolhida)
+invulnerabilidade por um round(exeto feitiços)
+carta de dano direto(arq mais ou menos 10)( guerreiro 20) (14)
+aumentar o range (only guerreiro, uma rodada)
+bloquear 1 turno de ataque 
+
+"""
+
+
+""""classe guerreiro
+deve possuir muita vida e um dano baixo
+classe arqueiro
+deve possuir uma vida baixa e um dano alto
+classe catapulta
+vida e danos medios """
+"""guerreiro tem que segurar em torno de 3 turno de ataques de um arqueiro """
+dano_do_arqueiro = 18,19,20,21,22,23,24,25
+vida_do_arqueiro = 18,17,16,15,14,13,12,11
+vida_do_guerreiro = 51,52,53,54,55,57,59,62
+ataque_do_guerreiro = 7,6,5,4,3,2,1,0
+dano_catapulta = 11,12,13,14,15,16,17,18
+vida_catapulta = 19,18,17,16,15,14,13,12
